4e32567f2c3bc1c933bdb053a408dde1efd15e7d
Reddy Leela Venkata Krishna Prasad <135614599+prasad924@users.noreply.github.com>
2024-02-14 06:40:54 +0530

    Update Sort.cpp

--- Lab Internal/Set7/Sort.cpp
+++ Lab Internal/Set7/Sort.cpp
@@ -1,60 +1,63 @@
-#include <iostream>
-#include <queue>
-#include <stack>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-bool canSortQueue(queue<int>& inputQueue, queue<int>& targetQueue) {
-    stack<int> tempStack;
-
-    while (!inputQueue.empty()) {
-        int current = inputQueue.front();
-        inputQueue.pop();
-
-        if (current == targetQueue.front()) {
-            targetQueue.pop();
-        } else {
-            if (tempStack.empty()) {
-                tempStack.push(current);
-            } else {
-                while (!tempStack.empty() && tempStack.top() < current) {
-                    targetQueue.push(tempStack.top());
-                    tempStack.pop();
-                }
-                tempStack.push(current);
-            }
-        }
-    }
-
-    while (!tempStack.empty() && tempStack.top() == targetQueue.front()) {
-        tempStack.pop();
-        targetQueue.pop();
-    }
-
-    return tempStack.empty() && targetQueue.empty();
+// Function to check if given queue element
+// can be sorted into another queue using a
+// stack.
+bool checkSorted(int n, queue<int>& q)
+{
+stack<int> st;
+int expected = 1;
+int fnt;
+// while given Queue is not empty.
+while (!q.empty()) {
+fnt = q.front();
+q.pop();
+// if front element isthe expected element
+if (fnt == expected)
+expected++;
+else {
+// ifstack is empty, push the element
+if (st.empty()) {
+st.push(fnt);
 }
-
-int main() {
-    queue<int> inputQueue, targetQueue;
-
-    // Populate the queues with data
-    inputQueue.push(3);
-    inputQueue.push(2);
-    inputQueue.push(1);
-    inputQueue.push(4);
-
-    targetQueue.push(1);
-    targetQueue.push(2);
-    targetQueue.push(3);
-    targetQueue.push(4);
-
-    if (canSortQueue(inputQueue, targetQueue)) {
-        cout << "The input queue can be sorted into the target queue using a stack.\n";
-    } else {
-        cout << "The input queue cannot be sorted into the target queue using a stack.\n";
-    }
-
-    return 0;
+// iftop element is less than element which
+// need to be pushed, then return false.
+else if (!st.empty() && st.top() < fnt) {
+return false;
 }
-
-
+// else push into the stack.
+else
+st.push(fnt);
+}
+// while expected element are coming from
+// stack, pop them out.
+while (!st.empty() && st.top() == expected) {
+st.pop();
+expected++;
+}
+}
+// ifthe final expected element value is equal
+// to initial Queue size and the stack is empty.
+68
+if (expected - 1 == n && st.empty())
+return true;
+return false;
+}
+// Driven Program
+int main()
+{
+queue<int> q;
+char ans;
+int val;
+do
+{
+cout<<"Enter value to be pushed:"<<endl;
+cin>>val;
+q.push(val);
+cout<<"Do you want to add another element : press[y for yes and n for no]:";
+cin>>ans;
+} while(ans=='y');
+int n = q.size();
+(checkSorted(n, q) ? (cout << "Yes") :(cout << "No"));
+return 0;
+}
